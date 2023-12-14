069f2a5699972ab3fe62f09d19746fb12a3cd62b
Andranik0 <anakinouki@hotmail.fr>
2023-12-14 11:18:05 +0100

    Update app_service.tf

--- terraform/azure/app_service.tf
+++ terraform/azure/app_service.tf
@@ -1,7 +1,7 @@
 resource azurerm_app_service_plan "example" {
   name                = "terragoat-app-service-plan-${var.environment}"
   location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = azurerm_resource_group.example.name 
 
   sku {
     tier = "Dynamic"
